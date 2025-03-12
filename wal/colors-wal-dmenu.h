b8de10a3fdf66836eb22705395df83db242395b2
edudscrc <eduardo.sa.coelho@gmail.com>
2025-03-11 22:01:42 -0300

    rofi, new colors, deleted wal folder

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c3c2c3", "#100c12" },
-	[SchemeSel] = { "#c3c2c3", "#6A4AA5" },
-	[SchemeOut] = { "#c3c2c3", "#93d1a4" },
-};
--- wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#100c12";
-static const char* selfgcolor   = "#c3c2c3";
-static const char* normbgcolor  = "#8A7497";
-static const char* normfgcolor  = "#c3c2c3";
-static const char* urgbgcolor   = "#6A4AA5";
-static const char* urgfgcolor   = "#c3c2c3";
